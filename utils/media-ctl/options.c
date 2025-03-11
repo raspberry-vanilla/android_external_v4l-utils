5e1b101a3fdafd54424b8463dcfbf10eba9122c1
Konsta <konsta09@gmail.com>
2025-03-11 02:16:23 +0200

    android: cec-ctl: media-ctl: v4l2-ctl: print pretty version

--- utils/media-ctl/options.c
+++ utils/media-ctl/options.c
@@ -46,10 +46,14 @@ static void print_version()
 {
 #define STR(x) #x
 #define STRING(x) STR(x)
+#ifndef ANDROID
 	printf("media-ctl %s%s\n", PACKAGE_VERSION, STRING(GIT_COMMIT_CNT));
 	if (strlen(STRING(GIT_SHA)))
 		printf("media-ctl SHA: %s %s\n",
 		       STRING(GIT_SHA), STRING(GIT_COMMIT_DATE));
+#else
+	printf("media-ctl %s\n", PACKAGE_VERSION);
+#endif
 }
 
 static void usage(const char *argv0)
