0c335c434bf52786227b51bf828d26aecbb42965
Konsta <konsta09@gmail.com>
2025-03-11 02:16:23 +0200

    android: cec-ctl: media-ctl: v4l2-ctl: print pretty version

--- utils/cec-ctl/cec-ctl.cpp
+++ utils/cec-ctl/cec-ctl.cpp
@@ -243,9 +243,13 @@ static void print_version()
 {
 #define STR(x) #x
 #define STRING(x) STR(x)
+#ifndef ANDROID
 	printf("cec-ctl %s%s\n", PACKAGE_VERSION, STRING(GIT_COMMIT_CNT));
 	if (strlen(STRING(GIT_SHA)))
 		printf("cec-ctl SHA: %s %s\n", STRING(GIT_SHA), STRING(GIT_COMMIT_DATE));
+#else
+	printf("cec-ctl %s\n", PACKAGE_VERSION);
+#endif
 }
 
 static void usage()
--- utils/v4l2-ctl/v4l2-ctl.cpp
+++ utils/v4l2-ctl/v4l2-ctl.cpp
@@ -301,10 +301,14 @@ static void print_version()
 {
 #define STR(x) #x
 #define STRING(x) STR(x)
+#ifndef ANDROID
 	printf("v4l2-ctl %s%s\n", PACKAGE_VERSION, STRING(GIT_COMMIT_CNT));
 	if (strlen(STRING(GIT_SHA)))
 		printf("v4l2-ctl SHA: %s %s\n",
 		       STRING(GIT_SHA), STRING(GIT_COMMIT_DATE));
+#else
+	printf("v4l2-ctl %s\n", PACKAGE_VERSION);
+#endif
 }
 
 int test_ioctl(int fd, unsigned long cmd, void *arg)
